ffaf7639f15b1d3e72aea3d07296e2aee421fe67
Jose Horacio <jhoracio19@hotmail.com>
2025-04-06 02:21:35 -0600

    haciendo commit en los avances

---
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-add-character',
+  templateUrl: './add-character.component.html',
+  styleUrls: ['./add-character.component.css']
+})
+export class AddCharacterComponent {
+
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
+
+  public character: Character = {
+    name: '',
+    power: 0
+  };
+
+  emitCharacter(): void{
+    console.log(this.character);
+
+    this.character.name = '';
+    this.character.power = 0;
+  
+  }
+
+}
